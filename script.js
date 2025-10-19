b1493a92307c1801e4bc65d615d252c6f37908e1
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-10-19 07:13:16 +0100

    Added task 0 dynamic to-do list file

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  const addButton = document.getElementById("add-task-btn")
+  const taskInput =  document.getElementById("task-input")
+  const taskList = document.getElementById("task-list")
+
+  function addTask (){
+    const taskText = taskInput.ariaValueMax.trim()
+    if(taskText != ""){
+      const li = document.createElement("li")
+      li.textContent = taskText
+      const removeBtn = document.createElement("button")
+      removeBtn.textContent = "Remove"
+      removeBtn.className = "remove-btn"
+      removeBtn.onclick = function(){
+        taskList.removeChild("li")
+      }
+      li.appendChild(removeBtn)
+      taskList.appendChild(li)
+      taskInput.value = ""
+
+       }
+      
+  }
+})
